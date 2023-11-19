eb289e0ec5b9a0576500ab75b7628936754f3ac8
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-19 20:01:19 +0000

    edit about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div
-      name="about"
-      className="w-full  h-screen px-4  mx-auto bg-slate-800"
-    >
+    <div name="about" className="w-full  h-screen px-4  mx-auto bg-slate-800">
       <div className="flex flex-col justify-center items-center w-full h-full ">
         <div
           className="max-w-[1000px] w-full grid grid-cols-2 
@@ -24,7 +21,7 @@ const About = () => {
               <h1 className="text-white text-2xl sm:text-4xl font-bold">
                 Ebrahim Beiati-Asl
               </h1>
-              <h2 className="text-[#dbdce0] text-2xl sm:text-4xl font-bold">
+              <h2 className="text-white text-lg text-center mb-8 block mt-1 leading-tight font-medium sm:text-4xl ">
                 I am Full Stack Developer
               </h2>
             </div>
