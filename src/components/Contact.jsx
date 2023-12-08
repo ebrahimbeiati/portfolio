f802702f93ad0b07081a67285681074d44435c8d
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-12-08 17:36:12 +0000

    edit contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,17 +51,6 @@ const Contact = () => {
           </ul>
         </div>
       </div>
-
-      {/* <div className="bg-gray-900 text-white py-6 w-full">
-        <div className="container mx-auto flex flex-col items-center">
-          <div className="text-2xl font-bold">Portfolio</div>
-
-          <p className="text-sm">
-            &copy; {new Date().getFullYear()} Ebrahim Beiati-Asl. All rights
-            reserved.
-          </p>
-        </div>
-      </div> */}
     </div>
   );
 };
