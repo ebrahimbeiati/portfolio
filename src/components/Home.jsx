0d0ce81be388e8ef50c1dbdf7ccd119d5abcdda2
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2025-02-21 20:35:14 +0000

    edit home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { HiArrowNarrowRight } from "react-icons/hi";
-// import { Link } from "react-scroll";
-
-// const Home = () => {
-//   return (
-//     <div name="home" className="flex flex-col justify-center  items-center h-screen bg-gradient-to-r from-blue-600 to-purple-800 text-white px-4">
-//       <h1 className="text-4xl leading-tight hover:underline  font-bold mb-6 text-center">
-//         Hi there, I'm <span className="text-blue-400">Ebrahim</span>
-//       </h1>
-//       <p className="text-lg text-center mb-8 block mt-1 leading-tight font-medium text-white hover:underline">
-//         As a passionate Software Engineer, I create innovative solutions to
-//         real-world problems.
-//       </p>
-
-//       <div className="w-full flex justify-center ">
-//         <Link to={"project"} smooth={true} duration={100}>
-//           <button
-//             className="text-white font-bold  border-2 px-6 py-4 my-2 flex items-center hover:bg-blue-800 hover:border-blue-800"
-//             type="button"
-//           >
-//             View Projects{" "}
-//             <span className="hover:rotate-90 duration-100 hover:cursor-s-resize">
-//               <HiArrowNarrowRight className="ml-3" />
-//             </span>
-//           </button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
